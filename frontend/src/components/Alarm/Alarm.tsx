e9d3ccbaa257be564f6f4677d7ce7d95b314da84
LEEJW1953 <jiwonlee1953@naver.com>
2024-07-19 12:52:49 +0900

    feat(header) : Header 컴포넌트 구현 #S11P12A309-107

--- frontend/src/components/Alarm/Alarm.tsx
+++ frontend/src/components/Alarm/Alarm.tsx
@@ -1,5 +1,5 @@
 import AlarmSVG from '@/assets/icons/alarm.svg?react';
 
 export function Alarm() {
-  return <AlarmSVG width={50} height={50} fill={'#fff'} />;
+  return <AlarmSVG width={24} height={24} fill={'#fff'} />;
 }
