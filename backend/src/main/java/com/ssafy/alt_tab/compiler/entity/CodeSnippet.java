cce92aa62c479b264a0949991092b0f00d831752
jijongkwon <help3451@naver.com>
2024-07-23 17:08:03 +0900

    feat(CodeSnippet) : CodeSnippet entity 생성 - code_snippet_id - language - code #S11P12A309-132

---
+++ backend/src/main/java/com/ssafy/alt_tab/compiler/entity/CodeSnippet.java
@@ -0,0 +1,26 @@
+package com.ssafy.alt_tab.compiler.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CodeSnippet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "code_snippet_id")
+    private Long id;
+    private String language;
+    private String code;
+}
