010a26eb276cb24cc1669497d88bc33cee155b1a
jijongkwon <help3451@naver.com>
2024-07-23 17:09:23 +0900

    feat(CodeExecutionRequestDto) : CodeExecutionRequest dto 작성 #S11P12A309-132

---
+++ backend/src/main/java/com/ssafy/alt_tab/compiler/dto/CodeExecutionRequestDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.alt_tab.compiler.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CodeExecutionRequestDto {
+    private String code;
+}
