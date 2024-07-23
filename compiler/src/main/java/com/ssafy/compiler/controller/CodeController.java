bb35015095706dc3770c9c6452c1747b7aa9d39b
jijongkwon <help3451@naver.com>
2024-07-23 17:01:52 +0900

    feat(CodeController) : controller 주석처리 - 나중 테스트를 위해 남겨둠 #S11P12A309-132

--- compiler/src/main/java/com/ssafy/compiler/controller/CodeController.java
+++ compiler/src/main/java/com/ssafy/compiler/controller/CodeController.java
@@ -1,14 +1,8 @@
 package com.ssafy.compiler.controller;
 
 
-import com.ssafy.compiler.dto.CodeExecutionRequestDto;
-import com.ssafy.compiler.service.CompileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class CodeController {
-
-    private final CompileService compileService;
-
-    @PostMapping("/execute")
-    public ResponseEntity<?> executeCode(@RequestBody CodeExecutionRequestDto request){
-        return new ResponseEntity<>(compileService.executeCode(request), HttpStatus.OK);
-    }
+//
+//    private final CompileService compileService;
+//
+//    @PostMapping("/execute")
+//    public ResponseEntity<?> executeCode(@RequestBody CodeExecutionRequestDto request){
+//        return new ResponseEntity<>(compileService.executeCode(request), HttpStatus.OK);
+//    }
 }
