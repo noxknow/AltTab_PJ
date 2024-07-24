65af3ead1af6312e57906b9e96b77bd98187f093
jijongkwon <help3451@naver.com>
2024-07-24 18:11:59 +0900

    feat(RabbitMQConfig) : RabbitMQConfig 설정 #S11P12A309-134

--- compiler/src/main/java/com/ssafy/compiler/common/config/RabbitMQConfig.java
+++ compiler/src/main/java/com/ssafy/compiler/common/config/RabbitMQConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.compiler.config;
+package com.ssafy.compiler.common.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
