38064ec29b22d42ed5ee763f0785d2b7386beba8
LEEJW1953 <jiwonlee1953@naver.com>
2024-07-25 18:01:33 +0900

    design : 모달 레이아웃 수정 #S11P12A309-167

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import CloseSVG from '@/assets/icons/close.svg?react';
 
 import styles from './Modal.module.scss';
@@ -5,12 +6,21 @@ import styles from './Modal.module.scss';
 type ModalProps = React.HTMLAttributes<HTMLDivElement> & {
   handleModalClose: () => void;
   modalContent: JSX.Element | undefined;
+  fill?: boolean;
 };
 
-export function Modal({ handleModalClose, modalContent }: ModalProps) {
+export function Modal({
+  handleModalClose,
+  modalContent,
+  fill = true,
+}: ModalProps) {
+  const modalClass = classNames(styles.modalContainer, {
+    [styles.fill]: fill,
+  });
+
   return (
     <>
-      <div className={styles.modalContainer}>
+      <div className={modalClass}>
         <button className={styles.closeButton} onClick={handleModalClose}>
           <CloseSVG width={24} height={24} stroke="#F24242" />
         </button>
