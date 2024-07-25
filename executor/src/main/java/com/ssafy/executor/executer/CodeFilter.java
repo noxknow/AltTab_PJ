bb6146c65b6fd5209d10b38e8d8ce6c5d137f1bb
jijongkwon <help3451@naver.com>
2024-07-25 15:29:13 +0900

    feat(CodeFilter) : 악의적 코드 필터 클래스 생성 - isSafeCode : boolean - getViolations : list #S11P12A309-151

---
+++ executor/src/main/java/com/ssafy/executor/executer/CodeFilter.java
@@ -0,0 +1,37 @@
+package com.ssafy.executor.executer;
+
+import com.ssafy.executor.common.enums.ForbiddenPattern;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeFilter {
+
+    /**
+     * 금지 패턴 탐색
+     * @param sourceCode
+     * @return 금지 패턴 일치 여부
+     */
+    public static boolean isSafeCode(String sourceCode) {
+        for (ForbiddenPattern pattern : ForbiddenPattern.values()) {
+            if (pattern.getPattern().matcher(sourceCode).find()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 금지 패턴 목록 조회
+     * @param sourceCode
+     * @return 금지 패턴 목록
+     */
+    public static List<String> getViolations(String sourceCode) {
+        List<String> violations = new ArrayList<>();
+        for (ForbiddenPattern pattern : ForbiddenPattern.values()) {
+            if (pattern.getPattern().matcher(sourceCode).find()) {
+                violations.add(pattern.getDescription());
+            }
+        }
+        return violations;
+    }
+}
