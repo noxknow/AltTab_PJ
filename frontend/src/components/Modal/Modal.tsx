1bf3bb411c1799c0a8c61905a2195d458d729ee2
LEEJW1953 <jiwonlee1953@naver.com>
2024-07-26 11:31:52 +0900

    feat(Compiler) : Compiler Modal 동적 렌더링 수정 #S11P12A309-167

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,22 +1,29 @@
 import classNames from 'classnames';
-import CloseSVG from '@/assets/icons/close.svg?react';
 
+import CloseSVG from '@/assets/icons/close.svg?react';
+import { useCompilerModalState } from '@/hooks/useCompilerState';
+import { MODAL } from '@/constants/Modal';
+
+import { ProblemModal } from './ProblemModal/ProblemModal';
+import { SolutionModal } from './SolutionModal/SolutionModal';
+import { CanvasModal } from './CanvasModal/CanvasModal';
+import { RunCodeModal } from './RunCodeModal/RunCodeModal';
 import styles from './Modal.module.scss';
 
 type ModalProps = React.HTMLAttributes<HTMLDivElement> & {
-  handleModalClose: () => void;
-  modalContent: JSX.Element | undefined;
-  fill?: boolean;
+  code: string;
 };
 
-export function Modal({
-  handleModalClose,
-  modalContent,
-  fill = true,
-}: ModalProps) {
+export function Modal({ code }: ModalProps) {
+  const { setIsModalOpen, modal, isFill } = useCompilerModalState();
+
   const modalClass = classNames(styles.modalContainer, {
-    [styles.fill]: fill,
+    [styles.fill]: isFill,
   });
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
@@ -24,7 +31,10 @@ export function Modal({
         <button className={styles.closeButton} onClick={handleModalClose}>
           <CloseSVG width={24} height={24} stroke="#F24242" />
         </button>
-        {modalContent}
+        {modal === MODAL.PROBLEM && <ProblemModal />}
+        {modal === MODAL.SOLUTION && <SolutionModal />}
+        {modal === MODAL.CANVAS && <CanvasModal />}
+        {modal === MODAL.RUN && <RunCodeModal code={code} />}
       </div>
     </>
   );
