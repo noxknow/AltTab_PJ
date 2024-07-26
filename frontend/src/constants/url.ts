d608c16498f8f863586449030235c1925546aebd
LEEJW1953 <jiwonlee1953@naver.com>
2024-07-26 15:02:18 +0900

    feat(api) : api 요청 경로 수정, .env 수정 #S11P12A309-176

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -1,3 +1,4 @@
 export const URL = {
   API: import.meta.env.VITE_API_URL,
+  API_LOCAL: import.meta.env.VITE_API_URL_LOCAL,
 };
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,16 +2,18 @@ import axios from 'axios';
 
 import { URL } from '@/constants/url';
 
+const baseURL =
+  import.meta.env.MODE === 'development' ? URL.API_LOCAL : URL.API;
+
+const apiVersion = '/v1';
+
 const headers = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
 };
 
 export const API = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:8080/api/v1'
-      : URL.API,
+  baseURL: baseURL + apiVersion,
   headers,
   withCredentials: true,
 });
