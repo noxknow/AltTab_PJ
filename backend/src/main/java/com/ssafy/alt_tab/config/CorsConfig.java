7f9575cedca31b5f294593865d4c08594f5ee83b
LEEJW1953 <jiwonlee1953@naver.com>
2024-07-26 13:38:30 +0900

    feat(CorsConfig) : CORS 설정 수정

--- backend/src/main/java/com/ssafy/alt_tab/config/CorsConfig.java
+++ backend/src/main/java/com/ssafy/alt_tab/config/CorsConfig.java
@@ -8,17 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-//    private final String DEVELOP_FRONT_ADDRESS = "http://70.12.246.179/:3000";
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
-//                .allowedOrigins(DEVELOP_FRONT_ADDRESS)
-//                .allowCredentials(true)
-                .allowedOrigins("*")
-                .allowCredentials(false)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .exposedHeaders("location")
-                .allowedHeaders("*");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
