2645911cbb527d4d1c8371eeb8b5b262530f3c1d
LEEJW1953 <jiwonlee1953@naver.com>
2024-07-29 13:14:25 +0900

    feat(code-highlight) : code highlight 적용 #S11P12A309-176

--- frontend/src/pages/Compiler/Compiler.tsx
+++ frontend/src/pages/Compiler/Compiler.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import styles from './Compiler.module.scss';
 import CanvasSection from '@/pages/Canvas/CanvasSection';
@@ -6,12 +6,14 @@ import { Button } from '@/components/Button/Button';
 import { Modal } from '@/components/Modal/Modal';
 import { useCompilerModalState } from '@/hooks/useCompilerState';
 import { MODAL } from '@/constants/Modal';
+import { highlightCode } from '@/utils/highlightCode';
 
 import { LineNumber } from './LineNumber';
 import { CompilerSidebar } from './CompilerSidebar';
 
 export function Compiler() {
   const [codeText, setCodeText] = useState('');
+  const [highlightedCode, setHighlightedCode] = useState('');
   const [canvasIsOpen, setCanvasIsOpen] = useState(false);
   const { isModalOpen, setIsModalOpen, setModal, setIsFill } =
     useCompilerModalState();
@@ -20,6 +22,10 @@ export function Compiler() {
     const newCodeText = event.target.value;
     setCodeText(newCodeText);
   };
+
+  useEffect(() => {
+    setHighlightedCode(highlightCode(codeText, 'java'));
+  }, [codeText]);
 
   const openCanvas = () => {
     setCanvasIsOpen(true);
@@ -37,19 +43,28 @@ export function Compiler() {
 
   return (
     <div className={styles.container}>
-      {isModalOpen && (
-        <Modal code={codeText} /> //handleModalClose={closeModal} modalContent={modal} />
-      )}
+      {isModalOpen && <Modal code={codeText} />}
       <div className={styles.compilerContainer}>
         <CompilerSidebar />
-        <div className={styles.compilerTitle}>Code Snippet</div>
+        <div className={styles.compilerTitle}>
+          <div>Code Snippet</div>
+          <div>Java</div>
+        </div>
         <div className={styles.compiler}>
           <div className={styles.compilerBody}>
             <LineNumber codeText={codeText} />
-            <textarea
-              className={styles.textArea}
-              onChange={handleChange}
-            ></textarea>
+            <div className={styles.codeContainer}>
+              <textarea
+                className={styles.textArea}
+                onChange={handleChange}
+              ></textarea>
+              <pre className={styles.codeArea}>
+                <code
+                  className={styles.code}
+                  dangerouslySetInnerHTML={{ __html: highlightedCode }}
+                />
+              </pre>
+            </div>
           </div>
         </div>
         <div className={styles.buttonContainer}>
