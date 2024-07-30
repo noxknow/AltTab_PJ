b03a49941b307681bc167b0aae4bbb2ceef79d3e
jijongkwon <help3451@naver.com>
2024-07-30 17:23:22 +0900

    fix(CodeExecutorAspect) : id null exception 해결 #S11P12A309-206

--- executor/src/main/java/com/ssafy/executor/common/aspect/CodeExecutorAspect.java
+++ executor/src/main/java/com/ssafy/executor/common/aspect/CodeExecutorAspect.java
@@ -1,6 +1,8 @@
 package com.ssafy.executor.common.aspect;
 
 import com.ssafy.executor.common.enums.ExceptionMessage;
+import com.ssafy.executor.common.exception.CompileException;
+import com.ssafy.executor.dto.CodeExecutionRequestDto;
 import com.ssafy.executor.dto.CodeExecutionResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -13,18 +15,33 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class CodeExecutorAspect {
     @Around("execution(* com.ssafy.executor.service.CodeExecutionService.executeCode(..))")
-    public Object handleCompileServiceExceptions(ProceedingJoinPoint joinPoint){
+    public Object handleCompileServiceExceptions(ProceedingJoinPoint joinPoint) {
         try {
             return joinPoint.proceed();
         } catch (Throwable ex) {
             log.error(ExceptionMessage.EXECUTION_FAILED.getMessage(), ex.getMessage());
-            return buildErrorResponse(ex);
+            return buildErrorResponse(ex, joinPoint);
         }
     }
 
-    private CodeExecutionResponseDto buildErrorResponse(Throwable ex) {
+    private CodeExecutionResponseDto buildErrorResponse(Throwable ex, ProceedingJoinPoint joinPoint) {
+        Long id = extractIdFromArgs(joinPoint.getArgs());
         return CodeExecutionResponseDto.builder()
+                .id(id)
                 .errorMessage(ex.getMessage())
                 .build();
+    }
+
+    private Long extractIdFromArgs(Object[] args) {
+        for (Object arg : args) {
+            if (arg instanceof CodeExecutionRequestDto) {
+                return ((CodeExecutionRequestDto) arg).getId();
+            }
+
+            if(arg instanceof CompileException){
+                return ((CompileException) arg).getId();
+            }
+        }
+        return null;
     }
 }
