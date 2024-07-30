4258260f22f138821b05fe09ed8c79d1a67aae22
jijongkwon <help3451@naver.com>
2024-07-30 10:09:34 +0900

    feat(CodeSnippetRepository) : 코드 스니펫 조회 기능 추가 #S11P12A309-206

--- backend/src/main/java/com/ssafy/alttab/compiler/repository/CodeSnippetRepository.java
+++ backend/src/main/java/com/ssafy/alttab/compiler/repository/CodeSnippetRepository.java
@@ -1,7 +1,9 @@
 package com.ssafy.alttab.compiler.repository;
 
 import com.ssafy.alttab.compiler.entity.CodeSnippet;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CodeSnippetRepository extends JpaRepository<CodeSnippet, Long> {
+    Optional<CodeSnippet> findById(Long id);
 }
