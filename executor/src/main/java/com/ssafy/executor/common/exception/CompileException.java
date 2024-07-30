4ed8208aa213b0964cc43bd5c2c68136169ac6dc
jijongkwon <help3451@naver.com>
2024-07-30 17:23:52 +0900

    fix(CompileException) : id null exception 해결 #S11P12A309-206

--- executor/src/main/java/com/ssafy/executor/common/exception/CompileException.java
+++ executor/src/main/java/com/ssafy/executor/common/exception/CompileException.java
@@ -1,11 +1,13 @@
 package com.ssafy.executor.common.exception;
 
-public class CompileException extends Exception {
-    public CompileException(String message) {
-        super(message);
-    }
+import lombok.Getter;
 
-    public CompileException(String message, Throwable cause) {
-        super(message, cause);
+@Getter
+public class CompileException extends Exception {
+    private final Long id;
+
+    public CompileException(String message, Long id) {
+        super(message);
+        this.id = id;
     }
 }
