ce134f3c18dac68054240213a892575b58b5fa2f
jijongkwon <help3451@naver.com>
2024-07-30 17:22:48 +0900

    fix(CodeExecutionResponseDto) : id null exception 해결 #S11P12A309-206

--- executor/src/main/java/com/ssafy/executor/dto/CodeExecutionResponseDto.java
+++ executor/src/main/java/com/ssafy/executor/dto/CodeExecutionResponseDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CodeExecutionResponseDto {
+    private Long id;
     private String output;
     private String errorMessage;
 }
