fc598f8588e6b35fcb94ab355ae191275b781141
jijongkwon <help3451@naver.com>
2024-07-30 10:08:40 +0900

    feat(CodeController) : 코드 실행 동기 방식 비동기 방식으로 변경 #S11P12A309-206

--- backend/src/main/java/com/ssafy/alttab/compiler/controller/CodeController.java
+++ backend/src/main/java/com/ssafy/alttab/compiler/controller/CodeController.java
@@ -1,10 +1,13 @@
 package com.ssafy.alttab.compiler.controller;
 
 import com.ssafy.alttab.compiler.dto.CodeExecutionRequestDto;
+import com.ssafy.alttab.compiler.dto.CodeExecutionResponseDto;
 import com.ssafy.alttab.compiler.service.CodeService;
-import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +21,13 @@ public class CodeController {
     private final CodeService codeService;
 
     @PostMapping("/execute")
-    public CompletableFuture<ResponseEntity<?>> executeCode(@RequestBody CodeExecutionRequestDto request) {
-        codeService.saveCode(request.getCode());
-        return codeService.executeCode(request)
-                .thenApply(ResponseEntity::ok);
+    public ResponseEntity<CodeExecutionResponseDto> executeCode(@RequestBody CodeExecutionRequestDto request) {
+        Long codeId = codeService.saveCode(request.getCode(), request.getId());
+        return new ResponseEntity<>(codeService.executeCodeAsync(request, codeId), HttpStatus.OK);
+    }
+
+    @GetMapping("/status/{id}")
+    public ResponseEntity<CodeExecutionResponseDto> getExecutionStatus(@PathVariable Long id) {
+        return new ResponseEntity<>(codeService.getExecutionResult(id), HttpStatus.OK);
     }
 }
