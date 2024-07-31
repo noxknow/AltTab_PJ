6e07464c700d03e1f9a65d6cc1a1261435100f2e
LEEJW1953 <jiwonlee1953@naver.com>
2024-07-31 16:14:41 +0900

    feat(Input) : Input 컴포넌트 구현 및 적용 #S11P12A309-242

--- frontend/src/pages/NewStudy/NewStudy.tsx
+++ frontend/src/pages/NewStudy/NewStudy.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/Button/Button';
 import { Tool } from '@/components/Tool/Tool';
 import { People } from '@/components/People/People';
 import { Info } from '@/components/Info/Info';
+import { Input } from '@/components/Input/Input';
 
 import styles from './NewStudy.module.scss';
 import { Check } from '@/components/Check/Check';
@@ -15,22 +16,21 @@ export function NewStudy() {
           <Tool />
           <div>
             <div className={styles.small_title}>팀명 생성</div>
-            <div className={styles.content}>팀명을 입력하세요</div>
+            <Input placeholder="팀명을 입력하세요" maxLength={15} />
           </div>
         </div>
         <div className={styles.option}>
           <People />
           <div>
             <div className={styles.small_title}>팀 초대</div>
-            <div className={styles.content}>팀원을 초대하세요</div>
+            <Input type="email" placeholder="팀원을 초대하세요" />
           </div>
         </div>
-
         <div className={styles.option}>
           <Info />
           <div>
             <div className={styles.small_title}>Info</div>
-            <div className={styles.content}>스터디를 소개해 주세요</div>
+            <Input placeholder="스터디를 소개해 주세요" maxLength={25} />
           </div>
         </div>
       </div>
