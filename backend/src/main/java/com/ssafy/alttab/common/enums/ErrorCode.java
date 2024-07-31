9ee02a9516a13de2da487bab4e45804899a25ade
chris2769 <chris2769@naver.com>
2024-07-31 15:25:28 +0900

    feat(exception) : Exception 설정 코드 변경 #S11P12A309-255

--- backend/src/main/java/com/ssafy/alttab/common/enums/ErrorCode.java
+++ backend/src/main/java/com/ssafy/alttab/common/enums/ErrorCode.java
@@ -13,7 +13,10 @@ public enum ErrorCode {
     ILLEGAL_REGISTRATION_ID(NOT_ACCEPTABLE, "illegal registration id"),
     TOKEN_EXPIRED(UNAUTHORIZED, "토큰이 만료되었습니다."),
     INVALID_TOKEN(UNAUTHORIZED, "올바르지 않은 토큰입니다."),
-    INVALID_JWT_SIGNATURE(UNAUTHORIZED, "잘못된 JWT 시그니처입니다.");
+    INVALID_JWT_SIGNATURE(UNAUTHORIZED, "잘못된 JWT 시그니처입니다."),
+
+    // Socket
+    CONNECTION_REFUSED(SERVICE_UNAVAILABLE, "Connection Refused");
 
     private final HttpStatus httpStatus;
     private final String message;
