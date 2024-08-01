a46f2bb65298c883840309ff3ea9ba1e48e9cf09
chris2769 <chris2769@naver.com>
2024-08-01 15:17:11 +0900

    chore(exception) : 빌드 에러 코드 ì수 추가 수저 #S11P12A309-258

--- backend/src/main/java/com/ssafy/alttab/common/exception/MemberException.java
+++ backend/src/main/java/com/ssafy/alttab/common/exception/MemberException.java
@@ -2,7 +2,7 @@ package com.ssafy.alttab.common.exception;
 
 import com.ssafy.alttab.common.enums.ErrorCode;
 
-public class MemberException extends CustomException {
+public class MemberException extends AppException {
 
     public MemberException(ErrorCode errorCode) {
         super(errorCode);
