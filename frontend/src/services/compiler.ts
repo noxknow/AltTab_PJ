c6421b11482891052b2ab22dbb7c94b5b0c9f97c
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-01 10:20:15 +0900

    feat : compiler 관련 api 추가 #S11P12A309-247

--- frontend/src/services/compiler.ts
+++ frontend/src/services/compiler.ts
@@ -3,13 +3,20 @@ import { API } from './api';
 
 export const compiler = {
   endpoint: {
-    default: '/compiler',
+    default: '/executor',
   },
 
   execute: async (form: requestCompiler) => {
     const { data } = await API.post(
       `${compiler.endpoint.default}/execute`,
       form,
+    );
+    return data;
+  },
+
+  status: async (studyId: string, problemId: string, problemTab: string) => {
+    const { data } = await API.get(
+      `${compiler.endpoint.default}/status/${studyId}/${problemId}/${problemTab}`,
     );
     return data;
   },
