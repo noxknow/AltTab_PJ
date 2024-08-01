2568847408ed18bc0a0e7274fb423ae3b99d6473
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-01 18:18:18 +0900

    feat(Compiler) : code tab 추가 #S11P12A309-249

--- frontend/src/pages/Compiler/Compiler.tsx
+++ frontend/src/pages/Compiler/Compiler.tsx
@@ -17,6 +17,16 @@ export function Compiler() {
   const [canvasIsOpen, setCanvasIsOpen] = useState(false);
   const { isModalOpen, setIsModalOpen, setModal, setIsFill } =
     useCompilerModalState();
+  // TODO : 스터디원 정보 받아오도록 수정
+  const [members] = useState([
+    '신승호',
+    '유경헌',
+    '이재영',
+    '이지원',
+    '이치왕',
+    '지종권',
+  ]);
+  const [selected, setSelected] = useState(0);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newCodeText = event.target.value;
@@ -46,6 +56,17 @@ export function Compiler() {
       {isModalOpen && <Modal code={codeText} />}
       <div className={styles.compilerContainer}>
         <CompilerSidebar />
+        <div className={styles.tabContainer}>
+          {members.map((member, index) => (
+            <div
+              key={index}
+              className={`${styles.tab} ${index === selected ? styles.selected : ''}`}
+              onClick={() => setSelected(index)}
+            >
+              {member}
+            </div>
+          ))}
+        </div>
         <div className={styles.compilerTitle}>
           <div>Code Snippet</div>
           <div>Java</div>
