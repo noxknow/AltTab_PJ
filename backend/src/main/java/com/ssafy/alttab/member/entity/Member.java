46a667310f910292dbe540661fba9fc417e2cce7
jijongkwon <help3451@naver.com>
2024-08-02 16:14:35 +0900

    feat(Member): 비즈니스 로직 구현 - 팔로우한 스터디 목록 #S11P12A309-236

--- backend/src/main/java/com/ssafy/alttab/member/entity/Member.java
+++ backend/src/main/java/com/ssafy/alttab/member/entity/Member.java
@@ -1,7 +1,12 @@
 package com.ssafy.alttab.member.entity;
 
 import com.ssafy.alttab.common.jointable.entity.MemberStudy;
+import com.ssafy.alttab.member.enums.MemberRoleStatus;
+import com.ssafy.alttab.study.entity.Problem;
+import com.ssafy.alttab.study.entity.StudyInfo;
+import com.ssafy.alttab.study.enums.ProblemStatus;
 import jakarta.persistence.*;
+import java.util.stream.Collectors;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -35,9 +40,23 @@ public class Member {
     private String memberHtmlUrl;
 
     @Column(nullable = false)
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private MemberRoleStatus role;
 
     @OneToMany(mappedBy = "member")
     private List<MemberStudy> memberStudies;
 
+    //==비즈니스 로직==//
+
+    /**
+     * 맴버가 팔로우 한 스터디 목록 반환
+     *
+     * @return
+     */
+    public List<StudyInfo> getFollowedStudies() {
+        return memberStudies.stream()
+                .filter(ms -> ms.getRole() == MemberRoleStatus.FOLLOWER)
+                .map(MemberStudy::getStudyInfo)
+                .collect(Collectors.toList());
+    }
 }
