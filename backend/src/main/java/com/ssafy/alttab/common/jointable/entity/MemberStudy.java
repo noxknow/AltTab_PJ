aa96e19b8e5bc4e0922dcac52ebf9a423a98e37f
jijongkwon <help3451@naver.com>
2024-08-02 16:11:59 +0900

    feat(MemberStudy): role string -> enum 수정 #S11P12A309-236

--- backend/src/main/java/com/ssafy/alttab/common/jointable/entity/MemberStudy.java
+++ backend/src/main/java/com/ssafy/alttab/common/jointable/entity/MemberStudy.java
@@ -1,13 +1,20 @@
 package com.ssafy.alttab.common.jointable.entity;
 
 import com.ssafy.alttab.member.entity.Member;
+import com.ssafy.alttab.member.enums.MemberRoleStatus;
 import com.ssafy.alttab.study.entity.StudyInfo;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MemberStudy {
     @Id
     private long memberStudyId;
@@ -17,8 +24,10 @@ public class MemberStudy {
     private Member member;
 
     @ManyToOne
-    @JoinColumn(name = "study_id")
+    @JoinColumn(name = "study_info_id")
     private StudyInfo studyInfo;
 
-    private String role;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberRoleStatus role;
 }
