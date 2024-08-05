c8ba7489730d776e4105a665454f54f781179155
jijongkwon <help3451@naver.com>
2024-08-05 13:10:01 +0900

    feat : sse 삭제 #S11P12A309-236

--- backend/src/main/java/com/ssafy/alttab/common/realtime/service/RealtimeUpdateService.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.alttab.common.realtime.service;
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-public interface RealtimeUpdateService<T> {
-    SseEmitter subscribe();
-    void sendUpdate(T data);
-}
