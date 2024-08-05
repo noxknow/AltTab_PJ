c4a6c4502acb882a2c92ef2edb43d62bfe75e2ac
jijongkwon <help3451@naver.com>
2024-08-05 13:13:18 +0900

    feat(CommunityController) : top solve, top follower study 반환 컨트롤러 구현 #S11P12A309-236

--- backend/src/main/java/com/ssafy/alttab/community/controller/CommunityController.java
+++ backend/src/main/java/com/ssafy/alttab/community/controller/CommunityController.java
@@ -1,9 +1,12 @@
 package com.ssafy.alttab.community.controller;
 
-import com.ssafy.alttab.common.realtime.service.WeeklyStudyUpdateService;
 import com.ssafy.alttab.community.dto.CommunityMainResponseDto;
+import com.ssafy.alttab.community.dto.TopFollowerDto;
+import com.ssafy.alttab.community.dto.TopSolverDto;
 import com.ssafy.alttab.community.service.CommunityService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/community")
+@RequiredArgsConstructor
 public class CommunityController {
 
     private final CommunityService communityService;
-    private final WeeklyStudyUpdateService weeklyStudyUpdateService;
 
-    @Autowired
-    public CommunityController(CommunityService communityService, WeeklyStudyUpdateService weeklyStudyUpdateService) {
-        this.communityService = communityService;
-        this.weeklyStudyUpdateService = weeklyStudyUpdateService;
-    }
 
     @GetMapping("/")
     public ResponseEntity<CommunityMainResponseDto> getCommunityMain() {
         CommunityMainResponseDto response = communityService.getCommunityMain();
-        return ResponseEntity.ok(response);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/top/follower")
+    public ResponseEntity<List<TopFollowerDto>> getTopFollower(){
+        return new ResponseEntity<>(communityService.getTopFollowerStudys(), HttpStatus.OK);
+    }
+
+    @GetMapping("/top/solve")
+    public ResponseEntity<List<TopSolverDto>> getTopSolver(){
+        return new ResponseEntity<>(communityService.getTopSolvers(), HttpStatus.OK);
     }
 }
