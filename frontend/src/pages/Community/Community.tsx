7560b948ef791715832c2f9f82adda36a70f83d1
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-07 12:49:05 +0900

    feat(Community) : 커뮤니티 페이지 필터 구현 #S11P12A309-377

--- frontend/src/pages/Community/Community.tsx
+++ frontend/src/pages/Community/Community.tsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+
 import { CommunityProfile } from '@/components/CommunityProfile/CommunityProfile';
+import { FILTER } from '@/constants/studyFilter';
 
 import styles from './Community.module.scss';
 
 export function Community() {
+  const [filter, setFilter] = useState(FILTER.SOLVED);
+  const solved = classNames(styles.filter, {
+    [styles.selected]: filter === FILTER.SOLVED,
+  });
+  const follower = classNames(styles.filter, {
+    [styles.selected]: filter === FILTER.FOLLOWER,
+  });
+
+  const handleFilterClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setFilter(e.currentTarget.name);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.weeklyStudies}>
@@ -15,8 +31,12 @@ export function Community() {
       </div>
       <div className={styles.topStudies}>
         <div className={styles.filterContainer}>
-          <div className={styles.filter}>푼 문제 수</div>
-          <div className={styles.filter}>팔로잉 수</div>
+          <button name={FILTER.SOLVED} onClick={handleFilterClick}>
+            <div className={solved}>푼 문제 수</div>
+          </button>
+          <button name={FILTER.FOLLOWER} onClick={handleFilterClick}>
+            <div className={follower}>팔로잉 수</div>
+          </button>
         </div>
         <div className={styles.cardContainer}>
           <CommunityProfile />
