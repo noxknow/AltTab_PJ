573b3022571afc03617cbcba977a1b5cea58f268
jijongkwon <help3451@naver.com>
2024-08-07 13:08:52 +0900

    feat (MemberRepository) : 메서드 명 변경 #S11P12A309-346

--- backend/src/main/java/com/ssafy/alttab/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ssafy/alttab/member/repository/MemberRepository.java
@@ -1,17 +1,12 @@
 package com.ssafy.alttab.member.repository;
 
 import com.ssafy.alttab.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUsername(String username);
 
-    Optional<Member> findByMemberEmail(String MemberEmail);
-
-    List<Member> findAllByMemberEmailIn(List<String> memberEmails);
+    Optional<Member> findByName(String name);
 }
