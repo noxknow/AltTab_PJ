03231ef56b43c8b0a9d2112a3eb10faa402a272e
jijongkwon <help3451@naver.com>
2024-08-07 13:10:11 +0900

    feat (GlobalExceptionHandler) : 사용자 못 찾을 시 exception 처리 #S11P12A309-346

--- backend/src/main/java/com/ssafy/alttab/common/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/alttab/common/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.ssafy.alttab.common.dto.ErrorResponseDto;
 import com.ssafy.alttab.common.exception.CodeNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,6 +23,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<?> handleGeneralException(Exception ex) {
+        return ErrorResponseDto.toResponse(ex);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<?> handleUsernameNotFoundException(UsernameNotFoundException ex) {
         return ErrorResponseDto.toResponse(ex);
     }
 }
