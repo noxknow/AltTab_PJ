dc9346c644f493c14a484e5e8853e7dc6b4eda00
이재영 <ehrlrkr@gmail.com>
2024-08-07 09:32:45 +0900

    feat(Attendance) : 스터디메인화면 중앙 하단 컴포넌트 #S11P12A309-221

--- frontend/src/components/StudyMain/Attendance.tsx
+++ frontend/src/components/StudyMain/Attendance.tsx
@@ -1,16 +1,27 @@
 import { AttendanceIcon } from '../AttendanceIcon/AttendanceIcon';
+import { Button } from '../Button/Button';
 import styles from './Attendance.module.scss';
+import Calendar from './Calendar';
 
 export default function Attendance() {
   return (
     <div className={styles.main}>
-      <div className={styles.header}>
-        <div>
-          <AttendanceIcon />
+      <div className={styles.top}>
+        <div className={styles.header}>
+          <div>
+            <AttendanceIcon />
+          </div>
+          <div>출석부</div>
         </div>
-        <div>출석부</div>
+        <div>
+          <Button color="green" fill={true} size="small">
+            출석하기
+          </Button>
+        </div>
       </div>
-      <div></div>
+      <div>
+        <Calendar />
+      </div>
     </div>
   );
 }
