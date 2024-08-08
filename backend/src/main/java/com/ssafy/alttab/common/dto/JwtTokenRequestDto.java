c48fa7c2f9151c507ddc085a9a2d43f7e7b0ebd5
jijongkwon <help3451@naver.com>
2024-08-08 12:48:35 +0900

    feat : 맴버 정보 불러오기 ( 임시 ) #S11P12A309-356

---
+++ backend/src/main/java/com/ssafy/alttab/common/dto/JwtTokenRequestDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.alttab.common.dto;
+
+import lombok.Getter;
+
+@Getter
+public class JwtTokenRequestDto {
+    private String jwt;
+}
--- backend/src/main/java/com/ssafy/alttab/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/alttab/member/controller/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +18,6 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/info")
     public ResponseEntity<?> getMemberInfo(@AuthenticationPrincipal UserDetails userDetails) throws MemberNotFoundException {
         return new ResponseEntity<>(memberService.getMemberInfo(userDetails.getUsername()), HttpStatus.OK);
     }
