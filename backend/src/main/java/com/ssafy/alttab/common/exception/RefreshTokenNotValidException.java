3f0f9859663e8d575be3d78d34c75b02d976b181
jijongkwon <help3451@naver.com>
2024-08-09 00:56:25 +0900

    feat(GlobalExceptionHandler) : TokenNotFoundException, TokenNotValidException 추가 #S11P12A309-356

--- backend/src/main/java/com/ssafy/alttab/common/exception/RefreshTokenNotValidException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.alttab.common.exception;
-
-public class RefreshTokenNotValidException extends RuntimeException{
-    public RefreshTokenNotValidException(String name){
-        super(name + "님의 토큰이 만료되었습니다.");
-    }
-}
