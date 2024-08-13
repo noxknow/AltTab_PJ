5db0707d2c5bc1335c6f1078045802002c658df6
jijongkwon <help3451@naver.com>
2024-08-13 15:28:12 +0900

    feat(ProblemResponseDto) : toDto overrode #S11P12A309-418

--- backend/src/main/java/com/ssafy/alttab/problem/dto/ProblemResponseDto.java
+++ backend/src/main/java/com/ssafy/alttab/problem/dto/ProblemResponseDto.java
@@ -1,12 +1,11 @@
 package com.ssafy.alttab.problem.dto;
 
+import com.ssafy.alttab.common.jointable.entity.ScheduleProblem;
 import com.ssafy.alttab.common.jointable.entity.StudyProblem;
 import com.ssafy.alttab.problem.entity.Problem;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDate;
 
 @Builder
 @AllArgsConstructor
@@ -20,6 +19,18 @@ public class ProblemResponseDto {
     private String presenter; // 발표자
 
     public static ProblemResponseDto toDto(StudyProblem studyProblem) {
+        Problem problem = studyProblem.getProblem();
+        return ProblemResponseDto.builder()
+                .studyProblemId(studyProblem.getId())
+                .problemId(problem.getProblemId())
+                .title(problem.getTitle())
+                .tag(problem.getTag())
+                .level(problem.getLevel())
+                .presenter(studyProblem.getPresenter())
+                .build();
+    }
+
+    public static ProblemResponseDto toDto(ScheduleProblem studyProblem) {
         Problem problem = studyProblem.getProblem();
         return ProblemResponseDto.builder()
                 .studyProblemId(studyProblem.getId())
