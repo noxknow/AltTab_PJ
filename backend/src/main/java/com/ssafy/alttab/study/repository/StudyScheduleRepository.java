b885fc5a47b21a3a46cc049b3bb153577ef04f0a
jijongkwon <help3451@naver.com>
2024-08-13 21:21:03 +0900

    feat(StudyScheduleRepository) : 년도월에 해당하는 deadlines 데이터 불러오기 #S11P12A309-421

--- backend/src/main/java/com/ssafy/alttab/study/repository/StudyScheduleRepository.java
+++ backend/src/main/java/com/ssafy/alttab/study/repository/StudyScheduleRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.alttab.study.repository;
 
 import com.ssafy.alttab.study.entity.StudySchedule;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,5 @@ public interface StudyScheduleRepository extends JpaRepository<StudySchedule, Lo
     @EntityGraph(attributePaths = {"ScheduleProblems", "ScheduleProblems.problem"})
     Optional<StudySchedule> findByStudyIdAndDeadline(Long studyId, LocalDate deadline);
 
+    List<StudySchedule> findAllByDeadlineBetween(LocalDate startDate, LocalDate endDate);
 }
