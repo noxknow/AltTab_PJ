5c0e2c03be8cea6b5dae21536d18c5127d1add06
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-13 11:13:38 +0900

    feat(Input) : Input 컴포넌트 수정 #S11P12A309-440

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,31 +1,6 @@
 import styles from './Input.module.scss';
 
-type InputProps = {
-  placeholder?: string;
-  maxLength?: number;
-  type?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  readonly?: boolean;
-};
-
-export function Input({
-  type = 'text',
-  placeholder,
-  maxLength,
-  value,
-  onChange,
-  readonly,
-}: InputProps) {
-  return (
-    <input
-      type={type}
-      className={styles.container}
-      placeholder={placeholder}
-      maxLength={maxLength}
-      value={value}
-      onChange={onChange}
-      readOnly={readonly}
-    />
-  );
+export function Input({ ...props }) {
+  const { type = 'text' } = props;
+  return <input type={type} className={styles.container} {...props} />;
 }
