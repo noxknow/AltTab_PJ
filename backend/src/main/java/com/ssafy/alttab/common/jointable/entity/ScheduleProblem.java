69a277ac27a9318d3f3f5b4ed821a5a68ae097ab
jijongkwon <help3451@naver.com>
2024-08-13 15:27:21 +0900

    feat(ScheduleProblem) : 스케줄 문제 관련된 entity 생성 #S11P12A309-418

---
+++ backend/src/main/java/com/ssafy/alttab/common/jointable/entity/ScheduleProblem.java
@@ -0,0 +1,54 @@
+package com.ssafy.alttab.common.jointable.entity;
+
+import com.ssafy.alttab.problem.entity.Problem;
+import com.ssafy.alttab.study.entity.StudySchedule;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ScheduleProblem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_schedule_id")
+    private StudySchedule studySchedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id")
+    private Problem problem;
+
+    private String presenter;
+
+    //== 생성 메서드 ==//
+    public static ScheduleProblem createStudySchedule(StudySchedule studySchedule, Problem problem, String presenter) {
+        ScheduleProblem scheduleProblem = ScheduleProblem.builder()
+                .studySchedule(studySchedule)
+                .problem(problem)
+                .presenter(presenter)
+                .build();
+        studySchedule.getScheduleProblems().add(scheduleProblem);
+        return scheduleProblem;
+    }
+
+    //== 비즈니스 로직 ==//
+    public void changeStudySchedule(StudySchedule studySchedule) {
+        this.studySchedule = studySchedule;
+    }
+}
