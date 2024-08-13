3a43870213cc86bd354f66ff4e37aef7a1686913
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-13 22:05:31 +0900

    feat(Following) : 팔로우 관련 api 연동 #S11P12A309-458

--- frontend/src/pages/Community/Community.tsx
+++ frontend/src/pages/Community/Community.tsx
@@ -7,26 +7,33 @@ import {
   useGetWeeklyStudiesQuery,
   useGetTopSolvedStudiesQuery,
   useGetTopFollowerStudiesQuery,
+  useGetFollowingStudiesQuery,
 } from '@/queries/community';
+import { useGetMyInfoQuery } from '@/queries/member';
 import { communityStudy } from '@/types/study';
+import { Loading } from '@/components/Loading/Loading';
 
 import styles from './Community.module.scss';
 
 export function Community() {
   const [filter, setFilter] = useState(FILTER.SOLVED);
   const [weeklyStudies, setWeeklyStudies] = useState<communityStudy[]>([]);
-  const [communityStudies, setCommunityStudies] = useState<communityStudy[]>(
-    [],
-  );
+  const [communityStudies, setCommunityStudies] = useState<communityStudy[]>();
+  const [isLoading, setIsLoading] = useState(false);
   const solved = classNames(styles.filter, {
     [styles.selected]: filter === FILTER.SOLVED,
   });
   const follower = classNames(styles.filter, {
     [styles.selected]: filter === FILTER.FOLLOWER,
   });
+  const following = classNames(styles.filter, {
+    [styles.selected]: filter === FILTER.FOLLOWING,
+  });
   const { data: initialStudies } = useGetWeeklyStudiesQuery();
   const { refetch: getTopSolvedStudies } = useGetTopSolvedStudiesQuery();
   const { refetch: getTopFollowerStudies } = useGetTopFollowerStudiesQuery();
+  const { refetch: getFollowingStudies } = useGetFollowingStudiesQuery();
+  const { isLogin } = useGetMyInfoQuery();
 
   useEffect(() => {
     const { weeklyStudies, topSolvers } = initialStudies
@@ -37,14 +44,25 @@ export function Community() {
   }, [initialStudies]);
 
   const getCommunityStudies = useCallback(async () => {
-    const { data } =
-      filter === FILTER.SOLVED
-        ? await getTopSolvedStudies()
-        : await getTopFollowerStudies();
-    setCommunityStudies(data!);
+    switch (filter) {
+      case FILTER.SOLVED:
+        const { data: TopSolvedStudies } = await getTopSolvedStudies();
+        setCommunityStudies(TopSolvedStudies!);
+        break;
+      case FILTER.FOLLOWER:
+        const { data: TopFollowerStudies } = await getTopFollowerStudies();
+        setCommunityStudies(TopFollowerStudies!);
+        break;
+      case FILTER.FOLLOWING:
+        const { data: FollowingStudies } = await getFollowingStudies();
+        setCommunityStudies(FollowingStudies!);
+        break;
+    }
+    setIsLoading(false);
   }, [filter]);
 
   useEffect(() => {
+    setIsLoading(true);
     getCommunityStudies();
   }, [filter]);
 
@@ -54,31 +72,42 @@ export function Community() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.weeklyStudies}>
-        <div>금주의 스터디</div>
-        <div className={styles.cardContainer}>
-          {weeklyStudies &&
-            weeklyStudies.map((study, index) => (
-              <CommunityProfile key={index} study={study} />
-            ))}
-        </div>
-      </div>
-      <div className={styles.topStudies}>
-        <div className={styles.filterContainer}>
-          <button name={FILTER.SOLVED} onClick={handleFilterClick}>
-            <div className={solved}>푼 문제 수</div>
-          </button>
-          <button name={FILTER.FOLLOWER} onClick={handleFilterClick}>
-            <div className={follower}>팔로잉 수</div>
-          </button>
-        </div>
-        <div className={styles.cardContainer}>
-          {communityStudies &&
-            communityStudies.map((study, index) => (
-              <CommunityProfile key={index} study={study} />
-            ))}
-        </div>
-      </div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className={styles.weeklyStudies}>
+            <div>금주의 스터디</div>
+            <div className={styles.cardContainer}>
+              {weeklyStudies &&
+                weeklyStudies.map((study, index) => (
+                  <CommunityProfile key={index} study={study} />
+                ))}
+            </div>
+          </div>
+          <div className={styles.topStudies}>
+            <div className={styles.filterContainer}>
+              <button name={FILTER.SOLVED} onClick={handleFilterClick}>
+                <div className={solved}>푼 문제 수</div>
+              </button>
+              <button name={FILTER.FOLLOWER} onClick={handleFilterClick}>
+                <div className={follower}>팔로워 수</div>
+              </button>
+              {isLogin && (
+                <button name={FILTER.FOLLOWING} onClick={handleFilterClick}>
+                  <div className={following}>팔로잉</div>
+                </button>
+              )}
+            </div>
+            <div className={styles.cardContainer}>
+              {communityStudies &&
+                communityStudies.map((study, index) => (
+                  <CommunityProfile key={index} study={study} />
+                ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
