0974dc1cefc87b9b97bd18322d714717a91a36ab
jijongkwon <help3451@naver.com>
2024-08-13 09:28:43 +0900

    feat(StudyScheduleRepository) : startDay, studyId로 스케줄 조회 #S11P12A309-418

--- backend/src/main/java/com/ssafy/alttab/study/repository/StudyScheduleRepository.java
+++ backend/src/main/java/com/ssafy/alttab/study/repository/StudyScheduleRepository.java
@@ -1,10 +1,11 @@
 package com.ssafy.alttab.study.repository;
 
 import com.ssafy.alttab.study.entity.StudySchedule;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyScheduleRepository extends JpaRepository<StudySchedule, Long> {
-    Optional<StudySchedule> findByIdAndStartDay(Long id, LocalDateTime startDate);
+    Optional<StudySchedule> findByStudyIdAndDeadline(Long studyId, LocalDate deadline);
+
 }
