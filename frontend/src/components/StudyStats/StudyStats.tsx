388143d94087fab0a5ebeaf8d7f6141bb704b655
chris2769 <chris2769@naver.com>
2024-08-13 18:21:33 +0900

    hotfix(StudyStats) : 통계 및 분석 에러 해결

--- frontend/src/components/StudyStats/StudyStats.tsx
+++ frontend/src/components/StudyStats/StudyStats.tsx
@@ -1,14 +1,18 @@
+import { useParams } from 'react-router-dom';
+
 import { StudyStatChart } from './StudyStatChart';
 import styles from './StudyStats.module.scss';
 import { StudyStatsHeader } from './StudyStatsHeader';
 import { StudyStatsInfo } from './StudyStatsInfo';
 
 export function StudyStats() {
+  const { studyId } = useParams();
+
   return (
     <div className={styles.main}>
       <StudyStatsHeader />
-      <StudyStatChart />
-      <StudyStatsInfo score={5247} solved={141} ranking={11} />
+      <StudyStatChart studyId={studyId!} />
+      <StudyStatsInfo studyId={studyId!} />
     </div>
   );
 }
