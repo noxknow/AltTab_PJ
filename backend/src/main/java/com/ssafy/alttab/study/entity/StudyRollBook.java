61c9abb262fb44429f04dc83861a0feaa0eebe82
yootal <khyoo96@naver.com>
2024-08-14 10:52:13 +0900

    feat(attend) : 출석부 #S11P12A309-406

---
+++ backend/src/main/java/com/ssafy/alttab/study/entity/StudyRollBook.java
@@ -0,0 +1,34 @@
+package com.ssafy.alttab.study.entity;
+
+import com.ssafy.alttab.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudyRollBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "study_roll_book_id")
+    private Long id;
+
+    private String memberName;
+    private Long studyId;
+    private LocalDate todayDate;
+
+    public static StudyRollBook createStudyRollBook(String name, Long studyId, LocalDate todayDate) {
+        return StudyRollBook.builder()
+                .memberName(name)
+                .studyId(studyId)
+                .todayDate(todayDate)
+                .build();
+    }
+}
