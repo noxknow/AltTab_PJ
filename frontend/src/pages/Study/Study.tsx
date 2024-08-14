6f670b5020ebcb3cae9ed424f3fd33142c2fad6f
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-14 23:08:48 +0900

    feat(Study) : 다른 스터디 페이지 이동 시 재렌더링 되지 않는 문제 해결 #S11P12A309-462

--- frontend/src/pages/Study/Study.tsx
+++ frontend/src/pages/Study/Study.tsx
@@ -1,11 +1,16 @@
-import styles from './Study.module.scss';
+import { useParams } from 'react-router-dom';
+
 import { StudyLeftBar } from '@/components/StudyLeftBar/StudyLeftBar';
 import { StudyStats } from '@/components/StudyStats/StudyStats';
 import { StudyMain } from '@/components/StudyMain/StudyMain';
 
+import styles from './Study.module.scss';
+
 export function Study() {
+  const { studyId } = useParams();
+
   return (
-    <div className={styles.main}>
+    <div key={studyId} className={styles.main}>
       <StudyLeftBar />
       <StudyMain />
       <StudyStats />
