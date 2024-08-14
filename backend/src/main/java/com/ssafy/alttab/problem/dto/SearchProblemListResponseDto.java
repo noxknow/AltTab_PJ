5c9e032e1f923b073f7e91cf054f39f03619d12b
chris2769 <chris2769@naver.com>
2024-08-14 11:17:10 +0900

    feat(SearchProblemResponseDto) : 문제 검색 결과 전달을 위한 dto 구현 #S11P12A309-449

--- backend/src/main/java/com/ssafy/alttab/problem/dto/SearchProblemListResponseDto.java
+++ backend/src/main/java/com/ssafy/alttab/problem/dto/SearchProblemListResponseDto.java
@@ -1,2 +1,15 @@
-package com.ssafy.alttab.problem.dto;public class SearchProblemListResponseDto {
+package com.ssafy.alttab.problem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SearchProblemListResponseDto {
+
+    List<SearchProblemResponseDto> problems;
 }
