073fd24ed8d2531ff3394fa8ed0a552f9ec09ce7
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-15 19:07:14 +0900

    hotfix(StudyProblems) : ClickedDateProvider 사용 범위 수정

--- frontend/src/pages/StudyProblems/StudyProblems.tsx
+++ frontend/src/pages/StudyProblems/StudyProblems.tsx
@@ -1,16 +1,20 @@
 import { StudyLeftBar } from '@/components/StudyLeftBar/StudyLeftBar';
-import styles from './StudyProblems.module.scss';
 import { TotalProblemList } from '@/components/TotalProblemList/TotalProblemList';
+import { ClickedDateProvider } from '@/contexts/clickedDate';
+
+import styles from './StudyProblems.module.scss';
 
 export function StudyProblems() {
   return (
-    <div className={styles.main}>
-      <div className={styles.left}>
-        <StudyLeftBar />
+    <ClickedDateProvider>
+      <div className={styles.main}>
+        <div className={styles.left}>
+          <StudyLeftBar />
+        </div>
+        <div className={styles.right}>
+          <TotalProblemList />
+        </div>
       </div>
-      <div className={styles.right}>
-        <TotalProblemList />
-      </div>
-    </div>
+    </ClickedDateProvider>
   );
 }
