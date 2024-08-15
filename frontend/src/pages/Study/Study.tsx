8a3ce1201885471ed6eb015eecbd365c00472ee3
LEEJW1953 <jiwonlee1953@naver.com>
2024-08-15 17:00:02 +0900

    feat(ClickedDateProvider) : ClickedDate 상태 사용 범위 수정 #S11P12A309-462

--- frontend/src/pages/Study/Study.tsx
+++ frontend/src/pages/Study/Study.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { StudyLeftBar } from '@/components/StudyLeftBar/StudyLeftBar';
 import { StudyStats } from '@/components/StudyStats/StudyStats';
 import { StudyMain } from '@/components/StudyMain/StudyMain';
+import { ClickedDateProvider } from '@/contexts/clickedDate';
 
 import styles from './Study.module.scss';
 
@@ -10,10 +11,12 @@ export function Study() {
   const { studyId } = useParams();
 
   return (
-    <div key={studyId} className={styles.main}>
-      <StudyLeftBar />
-      <StudyMain />
-      <StudyStats />
-    </div>
+    <ClickedDateProvider>
+      <div key={studyId} className={styles.main}>
+        <StudyLeftBar />
+        <StudyMain />
+        <StudyStats />
+      </div>
+    </ClickedDateProvider>
   );
 }
